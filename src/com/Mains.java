fd932ac426fe95f599295d401d5f5155d96279de
Traveller-g <72650702+Traveller-g@users.noreply.github.com>
2020-10-31 14:57:59 +0800

    Add files via upload

---
+++ src/com/Mains.java
@@ -0,0 +1,13 @@
+package com;
+
+
+public class Mains {
+	public static void main(String[] args) {
+		
+		
+		Test test = new Test();
+
+		test.show();
+		
+	}
+}
---
+++ src/com/Student.java
@@ -0,0 +1,14 @@
+package com;
+
+
+public class Student extends Personnel{
+	
+	Student(int id, int age, String name, String sex) {
+		super(id, age, name, sex);
+		// TODO Auto-generated constructor stub
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("\nÇëÊäÈëĞÕÃû£º");
+//		name=scanner.next();
+	}
+
+}
